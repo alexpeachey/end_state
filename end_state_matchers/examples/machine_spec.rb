24eb69d65dfc6a39f5d74f06cf3f5d4a103465b7
alexpeachey <alex.peachey@gmail.com>
2014-04-16 16:58:35 -0700

    Don't have a separate gem for matchers

--- end_state_matchers/examples/machine_spec.rb
+++
@@ -1,9 +0,0 @@
-$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
-require 'end_state_matchers'
-require_relative './machine'
-
-describe Machine do
-  specify { expect(Machine).to have_transition(a: :b).with_guard(Easy).with_finalizer(NoOp) }
-  specify { expect(Machine).to have_transition(a: :b).with_guards(Easy, Easy).with_finalizers(NoOp, NoOp) }
-  specify { expect(Machine).not_to have_transition(a: :c) }
-end
--- end_state_matchers/lib/end_state_matchers/version.rb
+++
@@ -1,3 +0,0 @@
-module EndStateMatchers
-  VERSION = "0.0.1"
-end
