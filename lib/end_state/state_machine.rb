6bc19b959a3295cef1170c9ddbd882d6d3ee169d
alexpeachey <alex.peachey@gmail.com>
2014-04-15 17:07:55 -0700

    Transitions now map from state to state
    * Now -> StateMachine.transition a: :b
    * Create class level state listing helpers
    * Create state! to transition

--- lib/end_state/state_machine.rb
+++ lib/end_state/state_machine.rb
@@ -1,13 +1,28 @@
 module EndState
   class StateMachine < SimpleDelegator
-    def self.transition(state, &block)
-      transition = Transition.new(state)
-      transitions[state] = transition
+    def self.transition(state_map, &block)
+      final_state = state_map.values.first
+      transition = Transition.new(final_state)
+      Array(state_map.keys.first).each do |state|
+        transitions[{ state => final_state }] = transition
+      end
       yield transition if block
     end
 
     def self.transitions
       @transitions ||= {}
+    end
+
+    def self.states
+      (start_states + end_states).uniq
+    end
+
+    def self.start_states
+      transitions.keys.map { |state_map| state_map.keys.first }.uniq
+    end
+
+    def self.end_states
+      transitions.keys.map { |state_map| state_map.values.first }.uniq
     end
 
     def object
@@ -16,8 +31,8 @@ module EndState
 
     def transition(state)
       previous_state = self.state
-      transition = self.class.transitions[state]
-      fail UnknownState, "The state: #{state} is unknown." unless transition
+      transition = self.class.transitions[{ previous_state => state }]
+      return block_transistion(transition, state) unless transition
       return false unless transition.guards_pass?(self)
       return false unless transition.action.new(self, state).call
       return false unless transition.finalize(self, previous_state)
@@ -25,13 +40,22 @@ module EndState
     end
 
     def method_missing(method, *args, &block)
+      check_state = method.to_s[0..-2].to_sym
+      super unless self.class.states.include? check_state
       if method.to_s.end_with?('?')
-        check_state = method.to_s[0..-2].to_sym
-        super unless self.class.transitions.keys.include? check_state
         state == check_state
+      elsif method.to_s.end_with?('!')
+        transition check_state
       else
         super
       end
+    end
+
+    private
+
+    def block_transistion(transition, state)
+      return false if self.class.end_states.include? state
+      fail UnknownState, "The state: #{state} is unknown." unless transition
     end
   end
 end
--- spec/end_state/state_machine_spec.rb
+++ spec/end_state/state_machine_spec.rb
@@ -8,21 +8,48 @@ module EndState
     before { StateMachine.instance_variable_set '@transitions'.to_sym, nil }
 
     describe '.transition' do
-      let(:state) { :a }
+      let(:state_map) { { a: :b } }
       let(:yielded) { OpenStruct.new(transition: nil) }
-      before { StateMachine.transition(state) { |transition| yielded.transition = transition } }
+      before { StateMachine.transition(state_map) { |transition| yielded.transition = transition } }
 
-      it 'yields a transition for the supplied state' do
-        expect(yielded.transition.state).to eq state
+      it 'yields a transition for the supplied end state' do
+        expect(yielded.transition.state).to eq :b
       end
 
       it 'does not require a block' do
-        expect(StateMachine.transition(:b)).not_to raise_error
+        expect(StateMachine.transition(b: :c)).not_to raise_error
       end
 
       it 'adds the transition to the state machine' do
-        expect(StateMachine.transitions[state]).to eq yielded.transition
+        expect(StateMachine.transitions[state_map]).to eq yielded.transition
       end
+    end
+
+    describe '.states' do
+      before do
+        StateMachine.transition(a: :b)
+        StateMachine.transition(b: :c)
+      end
+
+      specify { expect(StateMachine.states).to eq [:a, :b, :c] }
+    end
+
+    describe '.start_states' do
+      before do
+        StateMachine.transition(a: :b)
+        StateMachine.transition(b: :c)
+      end
+
+      specify { expect(StateMachine.start_states).to eq [:a, :b] }
+    end
+
+    describe '.end_states' do
+      before do
+        StateMachine.transition(a: :b)
+        StateMachine.transition(b: :c)
+      end
+
+      specify { expect(StateMachine.end_states).to eq [:b, :c] }
     end
 
     describe '#state' do
@@ -41,8 +68,8 @@ module EndState
 
     describe '#{state}?' do
       before do
-        StateMachine.transition :a
-        StateMachine.transition :b
+        StateMachine.transition a: :b
+        StateMachine.transition b: :c
       end
 
       context 'when the object has state :a' do
@@ -60,6 +87,18 @@ module EndState
       end
     end
 
+    describe '#{state}!' do
+      let(:object) { OpenStruct.new(state: :a) }
+      before do
+        StateMachine.transition a: :b
+      end
+
+      it 'transitions the state' do
+        machine.b!
+        expect(machine.state).to eq :b
+      end
+    end
+
     describe '#transition' do
       context 'when the transition does not exist' do
         it 'raises an unknown state error' do
@@ -68,18 +107,20 @@ module EndState
       end
 
       context 'when the transition does exist' do
+        before { object.state = :a }
+
         context 'and no configuration is given' do
-          before { StateMachine.transition :a }
+          before { StateMachine.transition a: :b }
 
           it 'transitions the state' do
-            machine.transition :a
-            expect(object.state).to eq :a
+            machine.transition :b
+            expect(object.state).to eq :b
           end
         end
 
         context 'and a guard is configured' do
           before do
-            StateMachine.transition :b do |transition|
+            StateMachine.transition a: :b do |transition|
               transition.allow_previous_states :a
             end
           end
@@ -105,7 +146,7 @@ module EndState
 
         context 'and a finalizer is configured' do
           before do
-            StateMachine.transition :b do |transition|
+            StateMachine.transition a: :b do |transition|
               transition.persistence_on
             end
           end
