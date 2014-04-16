5d146088878e1b554fd5c64f2c6e933ac9fdd262
alexpeachey <alex.peachey@gmail.com>
2014-04-16 13:05:23 -0700

    Add Additional Errors

--- lib/end_state/errors.rb
+++ lib/end_state/errors.rb
@@ -1,4 +1,7 @@
 module EndState
   class Error < StandardError; end
   class UnknownState < Error; end
+  class UnknownTransition < Error; end
+  class GuardFailed < Error; end
+  class FinalizerFailed < Error; end
 end
